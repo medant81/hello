85261e3b0b1056bed83ab9b5ae7bd31e265c6913
medant81 <medant81@gmail.com>
2024-05-21 19:05:29 +0700

    Vwerdion v1.4.0

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package hello
 
 import "fmt"
+import "github.com/medant81/hello/version1"
+import "github.com/medant81/hello/version2"
 
 func Hello() {
 	fmt.Println("Version v1.1.0")
+	version1.Version1()
+	version2.Version2()
 }
